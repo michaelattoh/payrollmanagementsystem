b8ac5b41265e7c75b6ba5c435f42b92fe332c1a6
Michael Attoh <47924435+rodmike751@users.noreply.github.com>
2021-11-25 12:19:04 +0000

    first commit

---
+++ about.php
@@ -0,0 +1,19 @@
+<div class="container-fluid " >
+			<div class="col-lg-12">
+				
+				<br />
+				<br />
+				<a class="btn btn-dark" href="http://localhost/payroll/index.php?page=home" role="button">Back</a>
+				<br />
+				<br />
+				<div class="card">
+					<div class="card-header">
+						<span><b>About</b></span>
+						
+					</div>
+                </div>
+				<!-- <div>
+					<img src="assets/img/error.png" alt="">
+					</div> -->
+            </div>
+</div>
---
+++ home.php
@@ -0,0 +1,41 @@
+<?php include 'db_connect.php' ?>
+<style>
+   
+</style>
+
+<div class="container-fluid">
+
+	<div class="row">
+		<div class="col-lg-12">
+			
+		</div>
+	</div>
+
+	<div class="row mt-3 ml-3 mr-3">
+			<div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                    <?php echo "Welcome back ". $_SESSION['login_name']."!"  ?>
+                                        
+                    </div>
+                </div>
+            </div>
+	</div>
+	<!-- <div class="row mt-3 ml-3 mr-3"> -->
+		<!-- <div class="col-lg-12"> -->
+			<!-- <div class="jumbotron"> -->
+  				<!-- <h1 class="display-4">Payroll Management System</h1> -->
+  				<!-- <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p> -->
+  				<!-- <hr class="my-4"> -->
+  				<!-- <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> -->
+ 				<!-- <p class="lead"> -->
+    			<!-- <a class="btn btn-primary btn-lg" href="http://localhost/payroll/index.php?page=payroll" role="button">Learn more</a> -->
+  				<!-- </p> -->
+			<!-- </div>	 -->
+		<!-- </div> -->
+	<!-- </div> -->
+
+</div>
+<script>
+	
+</script>
